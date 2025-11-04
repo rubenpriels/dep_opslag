067c03609597717049def2a1b5bf033352a35689
rubenpriels <ruben.priels@student.hogent.be>
2025-11-04 16:21:56 +0100

    Update lokalen_data_opkuisen.py

--- lokalen_data_opkuisen.py
+++ lokalen_data_opkuisen.py
@@ -2,23 +2,6 @@ import pandas as pd
 
 df = pd.read_csv(r"Data\filtered_lokalen.csv")
 
-# Rijen verwijderen waar TimeEdit = 0 is
-df = df[df["TimeEdit"] != 0]
-
-# kollomen verwijderen dat niet in de DWH moeten
-df = df.drop(columns=['PK_fmis_lokalen', 'Lokaal-ID', 'Lokaal-Beschrijving', 'Campus', 'Gebouw', 'Site-beschrijving', 'Gebouw-beschrijving', 'Verdieping-beschrijving', 'Locatie-ID', 'TimeEdit', 'Van', 'Tot'])
-
-# Hernoem de kolommen om zo in DWH te steken
-df.rename(columns={
-    'Code2': 'FullRoom',
-    'Code': 'Code',
-    'Verdieping': 'RoomFloor',
-    'Lokaal': 'Room',
-    'Categorie': 'Category',
-    'Oppervlakte': 'SurfaceArea',
-    'Capaciteit': 'Capacity',
-}, inplace=True)
-
 # floor omzetten naar nummer verdieping
 floor_map = {
     100: 0,
@@ -26,16 +9,17 @@ floor_map = {
     120: 2,
     130: 3,
     140: 4,
+    150: 5,
+    160: 6,
     90: -1
 }
 
-df["RoomFloor"] = df["RoomFloor"].map(floor_map).astype(int)
-
-# # Enkel GSCHB0.001 overhouden
-# df["FullRoom"] = df["FullRoom"].str.extract(r"(GSCHB\.-?\d+\.\d+)") # dit behouden in geval antwoord op vraag
+df["Verdieping"] = df["Verdieping"].map(floor_map).astype(int)
 
 # Zet Capacity om van float naar int
-df["Capacity"] = df["Capacity"].astype("Int64")
+df["Capaciteit"] = df["Capaciteit"].astype("Int64")
+
+df['Lokaal-Beschrijving'] = df['Lokaal-Beschrijving'].str.split().str[0]
 
 # Zet df om naar csv
 df.to_csv("Data/lokalendata_opgeschoond.csv", index=False)
